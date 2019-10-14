fb6ccf10c191b45d108dcefce01d718d00d412d5
amanvir uppal <amanvir.uppal@icloud.com>
2019-10-14 14:56:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,7 +15,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = [1, 2, 3, 4]
+  4 = array.pop
 end
 
 def remove_element_from_start_of_array(array)
